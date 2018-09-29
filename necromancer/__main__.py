87d5233ce491c2054aa9a8ed85803d5779fbdcb6
Zack Kollar (SeedyROM) <rallokkcaz@gmail.com>
2018-09-29 00:02:44 -0700

    Adding more slight touches before my brain dies

--- necromancer/__main__.py
+++ necromancer/__main__.py
@@ -1,4 +1,10 @@
-from necromancer.cli import CLI
+from docopt import docopt
+from necromancer import cli
+from necromancer.version import Version
+
+version = Version("0.0.1")
 
 if __name__ == '__main__':
-    CLI.parse_args()
+    args = docopt(cli.__doc__, version=version.number)
+    print(args)
+    cli.parsge_args(args)
--- necromancer/cli.py
+++ necromancer/cli.py
@@ -1,23 +1,25 @@
-import glob
-import json
-import os
-import sys
-import chalk
+"""Necromancer: A project boilerplate tool-kit!"
 
-from necromancer.template.config import (
-    ConfigException,
-    find_plugin,
-    load_plugin_config
-)
+Usage:
+  necro ship [r[aise]] <plugin-or-path>...
+  necro (-h | --help)
+  necro --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+import chalk
+from necromancer.template import config
 
 
 class CLI:
 
     @staticmethod
     def parse_args(**kwargs):
-
+        path = kwargs['path']
         try:
-            find_plugin(path)
-            load_plugin_config(path)
-        except ConfigException as e:
+            config.find_plugin(path)
+            config.load_plugin_config(path)
+        except config.ConfigException as e:
             print(chalk.red('Nonexistant or invalid template specified!'))
